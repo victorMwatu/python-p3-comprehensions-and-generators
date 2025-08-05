b01457080446067d661cb5e72dc5c6d85809b7f6
victorMwatu <victormwatu@gmail.com>
2025-08-05 16:08:28 +0300

    complete python p3 comprehensions and generatiors

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [i for i in num_list if i % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    l = [ word + "!" for word in sentence_list]
+    return l
